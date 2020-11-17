1b12d5e4682acc348c04cf38884149c47dcdfd42
Svilen Petrov <svilen.petrov97@gmail.com>
2020-11-17 15:15:43 +0000

    initial commit

---
+++ src/main/java/com/sparta/engineering72/sakilaproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sparta.engineering72.sakilaproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SakilaprojectApplication.class);
+    }
+
+}
