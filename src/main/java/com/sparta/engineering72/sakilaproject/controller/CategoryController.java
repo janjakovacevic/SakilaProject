a29ea2f5bf8c5b73f87f6bbda2bdd170af3f33bb
janjakovacevic <jaanjakoovacevic@gmail.com>
2020-11-19 09:36:23 +0000

    fixing categories bug

--- src/main/java/com/sparta/engineering72/sakilaproject/controller/CategoryController.java
+++ src/main/java/com/sparta/engineering72/sakilaproject/controller/CategoryController.java
@@ -25,18 +25,24 @@ public class CategoryController {
     }
 
     @GetMapping("/categories")
-    public String getCategories(ModelMap modelMap, @RequestParam(value = "genre", defaultValue = "ALL GENRE") String genre) {
-        List<Category> categories;
-        if (genre.equals("ALL GENRE")){
-            categories = categoryService.getAllCategories();
-        }
-        else {
-            categories = categoryService.getCategoryByCategoryName(genre);
-        }
-        modelMap.addAttribute("categories", categories);
+    public String getCategories(ModelMap modelMap) {
         modelMap.addAttribute("allCategories", categoryService.getAllCategories());
         return "/categories/categories";
     }
+
+//    @GetMapping("/categories")
+//    public String getCategories(ModelMap modelMap, @RequestParam(value = "genre", defaultValue = "ALL GENRE") String genre) {
+//        List<Category> categories;
+//        if (genre.equals("ALL GENRE")){
+//            categories = categoryService.getAllCategories();
+//        }
+//        else {
+//            categories = categoryService.getCategoryByCategoryName(genre);
+//        }
+//        modelMap.addAttribute("categories", categories);
+//        modelMap.addAttribute("allCategories", categoryService.getAllCategories());
+//        return "/categories/categories";
+//    }
 
     @GetMapping("/categories/details")
     public String getCategoryDetails(ModelMap modelMap,
